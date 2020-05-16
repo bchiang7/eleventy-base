d2e735a7b5263f81cb95a945323ecc7ea250da34
Brittany Chiang <brittany.chiang@gmail.com>
2020-05-16 14:19:20 -0400

    Rename a few things

--- src/_data/characters.js
+++
@@ -1,9 +0,0 @@
-// https://www.netlify.com/blog/2020/04/09/lets-learn-eleventy-boost-your-jamstack-skills-with-11ty/
-
-const axios = require('axios');
-
-module.exports = async () => {
-  const result = await axios.get('https://rickandmortyapi.com/api/character/');
-
-  return result.data.results;
-};
--- src/_data/github.js
+++ src/_data/github.js
@@ -1,11 +1,11 @@
-// FETCH GITHUB STARGAZERS COUNT (AND MORE) AT BUILD TIME
+// Fetch GitHub stars count at build time
 // https://www.11ty.dev/docs/quicktips/eliminate-js/
 
 const axios = require('axios');
 
 module.exports = async () => {
   // GitHub API: https://developer.github.com/v3/repos/#get
-  const result = await axios.get('https://api.github.com/repos/11ty/eleventy');
+  const result = await axios.get('https://api.github.com/repos/bchiang7/eleventy-base');
 
   return {
     stargazers: result.data.stargazers_count,
