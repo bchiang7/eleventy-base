a3c03acfd3ae7e417d32076b2a0815a3e5ca666f
Brittany Chiang <brittany.chiang@gmail.com>
2020-05-16 19:27:44 -0400

    Update styles

--- src/_data/github.js
+++ src/_data/github.js
@@ -1,13 +1,12 @@
 // Fetch GitHub stars count at build time
 // https://www.11ty.dev/docs/quicktips/eliminate-js/
 
-const axios = require('axios');
+// const axios = require('axios');
 
 module.exports = async () => {
   // GitHub API: https://developer.github.com/v3/repos/#get
-  const result = await axios.get('https://api.github.com/repos/bchiang7/eleventy-base');
-
-  return {
-    stargazers: result.data.stargazers_count,
-  };
+  // const result = await axios.get('https://api.github.com/repos/bchiang7/eleventy-base');
+  // return {
+  //   stargazers: result.data.stargazers_count,
+  // };
 };
